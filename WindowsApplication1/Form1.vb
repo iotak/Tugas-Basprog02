74fa8d4cb08085fbb982b8c58a00af02c636a8cc
iotak <voxraad@gmail.com>
2018-04-29 03:52:18 +0700

    Tugas Basprog
    Tugas Basprog

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim z, x, c, v As Single
+        z = TextBox2.Text
+        x = TextBox3.Text
+        c = TextBox4.Text
+        v = (z + x + c) / 3
+        Label7.Text = v
+        If v > 80 Then
+            Label8.Text = "Rp. 10.000.000"
+            MsgBox("Selemat Anda Mendapatkan Hadiah Rp. 10.000.000", MsgBoxStyle.Information, "Pesan")
+        ElseIf v > 70 Then
+            Label8.Text = "Rp. 7.500.000"
+            MsgBox("Selemat Anda Mendapatkan Hadiah Rp. 7.500.000", MsgBoxStyle.Information, "Pesan")
+        Else
+            Label8.Text = "Sertifikat"
+            MsgBox("Selemat Anda Mendapatkan Sertifikat", MsgBoxStyle.Information, "Pesan")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        Label7.Text = "........................................"
+        Label8.Text = "........................................"
+    End Sub
+End Class
---
+++ WindowsApplication1/Form2.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim a, b, c, d, f, g, h, i, j, k As Double
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = TextBox3.Text
+        d = TextBox4.Text
+        f = TextBox5.Text
+        g = TextBox6.Text
+        h = TextBox7.Text
+        i = TextBox8.Text
+
+        k = f + g + h + i
+
+        If k = 100 Then
+
+            j = ((a * f / 100) + (b * g / 100) + (c * h / 100) + (d * i / 100))
+            Label10.Text = j
+            If j >= 80 Then
+                Label11.Text = "A"
+            ElseIf j >= 70 Then
+                Label11.Text = "B"
+            ElseIf j >= 60 Then
+                Label11.Text = "C"
+            ElseIf j >= 50 Then
+                Label11.Text = "D"
+            Else
+                Label11.Text = "E"
+            End If
+        Else
+            MsgBox("Bobot Tidak Sama Dengan 100%", MsgBoxStyle.Exclamation, "Warning")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
